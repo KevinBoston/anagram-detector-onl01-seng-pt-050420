958b1c920133147eceecffc8677d548c6cf9e854
Kevin Boston <kbos22@gmail.com>
2020-05-25 14:13:21 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+class Anagram
+  #attr_accessor
+  def initialize(word)
+    @word = word 
+  end
+  
+  def match(arr)
+    
+    
+  end
+  
+  
+  
+  
+  
+  
+  
+  
+  
+end
